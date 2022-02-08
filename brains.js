213e8cc72202533aa823732daee91daf3fa74305
klousmu <67666128+klousmu@users.noreply.github.com>
2022-02-07 23:10:02 -0600

    Update brains.js

--- brains.js
+++ brains.js
@@ -10,17 +10,32 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   function addChat(input, product) {
-    const mainDiv = document.getElementById("main");
+    const messagesContainer = document.getElementById("messages");
+  
     let userDiv = document.createElement("div");
     userDiv.id = "user";
-    userDiv.innerHTML = `You: <span id="user-response">${input}</span>`;
-    mainDiv.appendChild(userDiv);
+    userDiv.className = "user response";
+    userDiv.innerHTML = `<img src="user.png" class="avatar"><span>${input}</span>`;
+    messagesContainer.appendChild(userDiv);
   
     let botDiv = document.createElement("div");
+    let botImg = document.createElement("img");
+    let botText = document.createElement("span");
     botDiv.id = "bot";
-    botDiv.innerHTML = `Chatbot: <span id="bot-response">${product}</span>`;
-    mainDiv.appendChild(botDiv);
-    speak(product);
-  }
-
+    botImg.src = "bot-mini.png";
+    botImg.className = "avatar";
+    botDiv.className = "bot response";
+    botText.innerText = "Typing...";
+    botDiv.appendChild(botText);
+    botDiv.appendChild(botImg);
+    messagesContainer.appendChild(botDiv);
+    // Keep messages at most recent
+    messagesContainer.scrollTop = messagesContainer.scrollHeight - messagesContainer.clientHeight;
   
+    // Fake delay to seem "real"
+    setTimeout(() => {
+      botText.innerText = `${product}`;
+      textToSpeech(product)
+    }, 2000
+    )
+}
